73951b53f96cdf79abe3a3a5eba46703cd6b7dd3
mike@mikestratton.net <mike@mikestratton.net>
2020-06-18 10:44:50 -0400

    3

---
+++ problem_3.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+*/
+
+function find_prime($value){
+    for($i=$value; $i>1; $i--){
+
+    }
+}
+
+find_prime();
