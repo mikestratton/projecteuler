818353d276715313575dfb2df3b3da31a6db30bc
mike@mikestratton.net <mike@mikestratton.net>
2020-06-18 11:41:41 -0400

    prob 3

--- problem_3.php
+++ problem_3.php
@@ -5,10 +5,31 @@
  * What is the largest prime factor of the number 600851475143 ?
 */
 
-function find_prime($value){
-    for($i=$value; $i>1; $i--){
 
+// php problem_3.php
+
+function IsPrime($n)
+{
+    for($x=2; $x<$n; $x++)
+    {
+        if($n %$x ==0)
+        {
+            return 0;
+        }
     }
+    return 1;
 }
-
-find_prime();
+$input = 0;
+$count = 0;
+while($input != 1){
+//    $var1 = 600851475143 - $count;
+    $var1 = 600851475067  - $count;
+    $var2 = IsPrime($var1);
+    print $var1 . " " . $var2 . "\n";
+    if($var2 == 1){
+        print $var2 . " is a prime \n";
+        $input = 1;
+        return $input;
+    }
+    $count++;
+}
